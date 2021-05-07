6016f47fa6fd59f7620e8a677ac0bafd4524f659
gattu <gattu@DESKTOP-D8HNM4S>
2021-05-07 13:46:36 +0530

    updated office idle code

--- src/test/java/sikulixpackage/OfficeIdleCode.java
+++ src/test/java/sikulixpackage/OfficeIdleCode.java
@@ -11,12 +11,19 @@ public class OfficeIdleCode
 		Screen s=new Screen();
 		while(2>1)
 		{
+			//Will work for screen coordinates
 			Location l1=new Location(500,500);
 			s.wheel(l1,Button.LEFT,0);
-			Thread.sleep(600000);
+			Thread.sleep(6000);
 			Location l2=new Location(800,500);
 			s.wheel(l2,Button.LEFT,0);
-			Thread.sleep(600000);
+			Thread.sleep(6000);
+			
+			//Will work for current mouse position
+			/*s.mouseMove(100,100);
+			Thread.sleep(6000);
+			s.mouseMove(-100,-100);
+			Thread.sleep(6000);*/
 		}
 	}
 }
